8c2af3815e1ec42146fe72c2fad0e4d7bf1ba81d
Bony Serge <sergebruno378@gmail.com>
2022-12-12 16:31:50 +0000

    new env

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,5 @@
-# from django.conf import settings
-# from django.conf.urls.static import static
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -11,6 +11,6 @@ urlpatterns = [
     path("auth/", include("djoser.urls.jwt")),
 ]
 
-# if settings.DEBUG:
-#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
