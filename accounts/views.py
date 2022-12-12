e9d9dd3dc32e73a0df901ccaf698f4b2ac5775d0
Bony Serge <sergebruno378@gmail.com>
2022-12-12 15:47:36 +0000

    new env

--- accounts/views.py
+++ accounts/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 
 def userActivation(request, uid, token):
     requests.post(
-        "http://127.0.0.1:8000/auth/users/activation/",
+        f"{request.scheme}://{request.get_host()}/auth/users/activation/",
         data={"uid": uid, "token": token},
     )
     return render(request, "activate.html")
