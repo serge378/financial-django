0bc8433d0cdd129e0b67e9e08179976e1af1b3dd
Bony Serge <sergebruno378@gmail.com>
2022-12-12 15:34:01 +0000

    new env

--- core/settings.py
+++ core/settings.py
@@ -57,7 +57,6 @@ MIDDLEWARE = [
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
-    "django_browser_reload.middleware.BrowserReloadMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
