bfabc67b9841a027509ae33010219bbd99bb0e0e
Bony Serge <sergebruno378@gmail.com>
2022-12-13 11:42:58 +0000

    create admin

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,6 @@
 # build_files.sh
 python3.9 -m pip install -r requirements.txt
 # python3.9 manage.py collectstatic --noinput
+export DJANGO_SUPERUSER_EMAIL=admin@admin.com
+export DJANGO_SUPERUSER_PASSWORD=test@pass1234
+python3.9 manage.py createsuperuser --no-input
