f8e609ec740960da887de2d178a9e31f9514ba0f
Bony Serge <sergebruno378@gmail.com>
2022-12-13 09:21:58 +0000

    manage staticfiles

--- core/settings/prod.py
+++ core/settings/prod.py
@@ -30,4 +30,4 @@ CORS_ORIGIN_ALLOW_ALL = False
 
 CORS_ORIGIN_WHITELIST = ("https://financial-performance-ci.vercel.app",)
 
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
